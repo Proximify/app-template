25f837b896e3ee3fe2cebf36b6d0d2e3908889ec
Diego Macrini <macrini@proximify.ca>
2020-11-14 12:18:46 -0500

    Testing plugin

--- src/AppCLI.php
+++ src/AppCLI.php
@@ -4,19 +4,23 @@ namespace Proximify;
 
 class AppCLI extends CLIActions
 {
+    private $handle;
+
     /**
      * @inheritDoc
      * 
      * @see https://github.com/composer/composer/blob/master/src/Composer/Config.php
      */
-    public function runComposerAction(array $options): void
+    public function runComposerAction(array $options, array $env): void
     {
+        // $this->startServer();
+
         // postPackageInstall(PackageEvent $event)
         // $installedPackage = $event->getOperation()->getPackage();
 
-        $env = $options[self::ENV_OPTIONS];
-        $event = $env['event'];
-        $env['event'] = 'EVENT';
+        //$env = $options[self::ENV_OPTIONS];
+        // $event = $env['event'] ?? false;
+        $env['event'] = isset($env['event']) ? 'EVENT' : 'NO EVENT';
 
         print_r($env);
 
@@ -33,11 +37,11 @@ class AppCLI extends CLIActions
                 $ns = $pkg->getNamespace();
 
                 if (self::startsWith($ns, 'Proximify')) {
-                    $own[] = [
-                        'name' => substr($name, strpos($name, '/') + 1),
-                        'type' => $pkg->getType(),
-                        'version' => $pkg->getVersion()
-                    ];
+                    // $own[] = [
+                    //     'name' => substr($name, strpos($name, '/') + 1),
+                    //     'type' => $pkg->getType(),
+                    //     'version' => $pkg->getVersion()
+                    // ];
                 }
             }
         }
@@ -45,11 +49,34 @@ class AppCLI extends CLIActions
         foreach ($own as $key => $pkg) {
             echo "\n > ", print_r($pkg, true);
         }
+
+        if ($env['name'] == 'post-update-cmd') {
+            $this->stopServer();
+        }
     }
 
     public static function startsWith(string $haystack, string $needle): bool
     {
         // https://github.com/symfony/polyfill/blob/master/src/Php80/Php80.php
         return 0 === strncmp($haystack, $needle, strlen($needle));
+    }
+
+    function startServer($port = '8080', $target = 'public')
+    {
+        if (!$this->handle) {
+            $domain = "localhost:$port";
+            $this->handle = proc_open("php -S $domain -t '$target'", [], $pipes);
+        }
+
+        return $this->handle;
+    }
+
+    function stopServer()
+    {
+        if ($this->handle) {
+            echo 'CLOSING';
+            proc_close($this->handle);
+            $this->handle = null;
+        }
     }
 }
